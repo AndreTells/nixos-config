924cf3d1d4fd5bffe478a293c481e316ee2a47ad
AndreTells <andresilvatelles@gmail.com>
2026-01-24 11:10:35 -0300

    configured xdg portal choser command for screen cast in wayland

--- modules/dwl.nix
+++ modules/dwl.nix
@@ -23,7 +23,7 @@ in
     foot # basic terminal expected by current dwl config (see termcmd in config.h)
     grim # taking screenshots
     slurp # allows one to select a region of the screen in wayland, it's usefull
-          # to take screenshots
+          # to take screenshots and to share the screen 
 
     wl-clipboard # system clipboard in wayland
     brightnessctl # screen brightness control interface
@@ -36,7 +36,29 @@ in
     #notification
     #dunst # notification daemon 
     libnotify
+
+    # wayland debugging tools
+    wlr-randr
+    wayland-utils
   ];
+
+  #allows sandboxed applications to access system resources in a secure manner
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr # dwl is based on wlroots so this is necessary 
+    ];
+    wlr = {
+      enable = true;
+      settings = { # uninteresting for this problem, for completeness only
+        screencast = {
+          max_fps = 30;
+          chooser_type = "simple";
+          chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Monitor: %o' -or";
+        };
+      };
+    };
+  };
 
   environment.etc."dwl_aux_scripts" = {
     enable = true;
