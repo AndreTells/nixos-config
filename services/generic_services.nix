8bd6f2d0158db4bf5c4a17a31ae28674b2357e66
AndreTells <andresilvatelles@gmail.com>
2026-01-24 11:09:49 -0300

    added screen capture module to pipewire

--- services/generic_services.nix
+++ services/generic_services.nix
@@ -12,6 +12,18 @@
       pulse.enable = true;
       alsa.enable = true;
       wireplumber.enable = true;
+      # Critical: Enable screen capture modules
+      extraConfig = {
+        # Add screen capture module
+        pipewire = {
+          "context.modules" = [
+            {
+              name = "libpipewire-module-screen-capture";
+              args = { };
+            }
+          ];
+        };
+      };
     };
 
   };
