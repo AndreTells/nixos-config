2959ace923d4cfa2af29d0a81225c85b8914958e
AndreTells <andresilvatelles@gmail.com>
2026-01-24 11:09:00 -0300

    changed dwl startup command to new script + set necessary environment variables to ensure apps use wayland

--- config/dwl_aux_scripts/start_wm.sh
+++ config/dwl_aux_scripts/start_wm.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-dwl -s "/etc/dwl_aux_scripts/start_quickshell.sh"
+export XDG_CURRENT_DESKTOP=wlroots # sets the desktop environment to be used by
+                                   # the current session, which, since dwl has
+                                   # no specific portal, is wlroots which dwl is
+                                   # based on
+                                   
+export MOZ_ENABLE_WAYLAND=1 # forces firefox to use wayland
+
+export QT_QPA_PLATFORM=wayland # forces qt applications to use the wayland backend
+
+export SDL_VIDEODRIVER=wayland # sets the video driver for "Simple DirectMedia
+                               # Layer" (SDL), which is used to expose low level
+                               # access to, among other things, graphics
+                               # hardware to applications. 
+export _JAVA_AWT_WM_NONREPARENTING=1 # makes java handles windows that don't get
+                                     # reparented in window managers that lack
+                                     # the behaviour, which is the case for dwl 
+dwl -s "/etc/dwl_aux_scripts/dwl_startup.sh"
